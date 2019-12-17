6153bdc02caa58a7fe0a749319e21e725254c128
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2019-12-17 13:29:00 -0600

    FileLoader extension

--- multidataparser/src/main/java/ni/devotion/multidataparser/file/FileLoader.kt
+++ multidataparser/src/main/java/ni/devotion/multidataparser/file/FileLoader.kt
@@ -1,6 +1,5 @@
 package ni.devotion.multidataparser.file
 
-import android.app.NotificationChannel
 import android.content.Context
 import android.util.Log
 import android.util.LruCache
@@ -12,6 +11,10 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class FileLoader internal constructor(private val context: Context)  {
+    companion object {
+        private lateinit var INSTANCE: FileLoader
+    }
+
     private val tag = "ttt FileLoader"
     private lateinit var fileType: FileTypes
     private var fileName = ""
@@ -30,109 +33,64 @@ class FileLoader internal constructor(private val context: Context)  {
                 return (file.length() / 1024).toInt()
             }
         }
-
         executorService = Executors.newFixedThreadPool(5, FileManager.FileThreadFactory())
-    }
-
-    companion object {
-        private var INSTANCE: FileLoader? = null
     }
 
     @Synchronized
     internal fun getInstance(): FileLoader{
-//        return INSTANCE ?: FileLoader(
-//            context
-//        ).also {
-//            INSTANCE = it
-//        }
-
         INSTANCE = FileLoader(context)
-        return INSTANCE!!
+        return INSTANCE
     }
 
     fun setCacheSize(cacheSizeInMB: Int): FileLoader {
-        var cacheSize = cacheSizeInMB * 1024 * 1024
-
-        if(cacheSize in 1 until maxCacheSize){
-            maxCacheSize = cacheSize
+        when (val cacheSize = cacheSizeInMB * 1024 * 1024) {
+            in 1 until maxCacheSize -> { maxCacheSize = cacheSize }
         }
-
-        return INSTANCE!!
+        return INSTANCE
     }
 
     fun setFileName(fileName: String): FileLoader {
         this.fileName = fileName
-
-        return INSTANCE!!
+        return INSTANCE
     }
 
     fun setCacheEnabled(isCacheEnabled: Boolean): FileLoader {
         this.isCacheEnabled = isCacheEnabled
-
-        return INSTANCE!!
+        return INSTANCE
     }
 
     fun setNotificationEnabled(isNotificationEnabled: Boolean, channelId: String): FileLoader {
-        require(channelId.isNotEmpty()){
-            "FileLoader:setNotificationEnabled - channelId should not be empty"
-        }
-
+        require(channelId.isNotEmpty()){ "FileLoader:setNotificationEnabled - channelId should not be empty" }
         this.isNotificationEnabled = isNotificationEnabled
         this.notificationChannelId = channelId
-
-        return INSTANCE!!
+        return INSTANCE
     }
 
     fun download(fileUrl: String, fileType: FileTypes) {
         this.fileType = fileType
         Log.d(tag, "download")
-        require(fileUrl.isNotEmpty()) {
-            "FileLoader:load - Url should not be empty"
-        }
-
+        require(fileUrl.isNotEmpty()) { "FileLoader:load - Url should not be empty" }
         if(fileName.isEmpty()) fileName = System.currentTimeMillis().toString()
-
         val file = checkFileInCache(fileUrl)
+        file ?: executorService.submit(FileLoadingThread(Files(fileUrl, fileType, fileName)))
         file?.let {
-            fileLoaderUtility?.saveFile(file,
-                Files(fileUrl, fileType, fileName),
-                onDownloadResultListener
-            )
-        } ?: run {
-            executorService.submit(FileLoadingThread(
-                Files(
-                    fileUrl,
-                    fileType,
-                    fileName
-                )
-            ))
+            fileLoaderUtility.saveFile(it, Files(fileUrl, fileType, fileName), onDownloadResultListener)
         }
     }
 
     @Synchronized
     private fun checkFileInCache(fileUrl: String): File? = memoryCache.get(fileUrl)
-
-    internal inner class FileLoadingThread(private var fileModel: Files) : Runnable {
-
-        override fun run() {
-            Log.d(tag, "FileLoadingThread")
-            val filepath =
-                if(isNotificationEnabled) fileLoaderUtility.downloadFileFromURL(fileModel, notificationChannelId, onDownloadResultListener)
-                else fileLoaderUtility.downloadFileFromURL(fileModel, onDownloadResultListener)
-
-            if(!isCacheEnabled) return
-            memoryCache.put(fileModel.fileUrl, File(filepath!!))
-
-        }
+        internal inner class FileLoadingThread(private var fileModel: Files) : Runnable {
+            override fun run() {
+                Log.d(tag, "FileLoadingThread")
+                val filepath = if(isNotificationEnabled) fileLoaderUtility.downloadFileFromURL(fileModel, notificationChannelId, onDownloadResultListener) else fileLoaderUtility.downloadFileFromURL(fileModel, onDownloadResultListener)
+                if(!isCacheEnabled) return
+                memoryCache.put(fileModel.fileUrl, File(filepath!!))
+            }
     }
 
-    interface OnDownloadResultListener{
-        fun onSuccess(filePath: String)
-        fun onError(error:String = "unknown error", errorCode:Int = 0)
-    }
-
-    fun setOnDownloadResultListener(onDownloadResultListener: OnDownloadResultListener): FileLoader{
+    fun setOnDownloadResultListener(onDownloadResultListener: OnDownloadResult): FileLoader{
         this.onDownloadResultListener = onDownloadResultListener
-        return INSTANCE!!
+        return INSTANCE
     }
 }
